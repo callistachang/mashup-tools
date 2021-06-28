c42434450854c51d7d0b31eddb3a78deb49a3931
Callista Chang <changcallista@gmail.com>
2021-06-28 13:47:50 +0800

    command line arguments + use first 10s of the song for faster beans
    closes #3

--- get_features.py
+++ get_features.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy.linalg
 import scipy.stats
 import warnings
+import argparse
 
 warnings.filterwarnings(action="ignore")
 
@@ -38,8 +39,10 @@ class Song:
     ]
     NOTE_NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
-    def __init__(self, audio_path):
-        self.audio_time_series, self.audio_sr = librosa.load(audio_path)
+    def __init__(self, audio_path, duration=10):
+        self.audio_time_series, self.audio_sr = librosa.load(
+            audio_path, duration=duration
+        )
 
     def get_pitch_class_distribution(self):
         """
@@ -54,7 +57,8 @@ class Song:
         """
         If the tempo varies, it gets the mean.
         """
-        return np.mean(librosa.beat.tempo(self.audio_time_series, sample_rate))
+        onset = librosa.onset.onset_strength(self.audio_time_series, self.audio_sr)
+        return np.mean(librosa.beat.tempo(onset_envelope=onset, sr=self.audio_sr))
 
     def _return_zscore(self, array):
         return scipy.stats.zscore(array)
@@ -80,7 +84,7 @@ class Song:
         # chroma vector . coefficients
         major_X = major_profile_rotated.T.dot(X)
         minor_X = minor_profile_rotated.T.dot(X)
-        print(major_X, minor_X)
+        # print(major_X, minor_X)
 
         major_winner = np.argmax(major_X)
         minor_winner = np.argmax(minor_X)
@@ -93,13 +97,35 @@ class Song:
             return f"{self.NOTE_NAMES[major_winner]} major or {self.NOTE_NAMES[minor_winner]} minor"
 
 
-if __name__ == "__main__":
-    audio_filepath = "./test_data/sam_smith.mp3"
-    print(audio_filepath)
-    song = Song(audio_filepath)
-    print(song.get_estimated_song_key())
+parser = argparse.ArgumentParser(description="Mashup Tools")
+parser.add_argument(
+    "-f",
+    "--filepath",
+    type=str,
+    dest="audio_filepath",
+    default="./test_data/openmind.mp3",
+    help="Audio filepath, e.g. path/to/file/song.mp3",
+)
+parser.add_argument("-k", "--key", dest="get_key", action="store_true", help="Get key")
+parser.add_argument(
+    "-s",
+    "--fullsong",
+    dest="is_full_song",
+    action="store_true",
+    help="Uses the full song for evaluation (default: first 10 seconds of song)",
+)
+parser.add_argument(
+    "-b",
+    "--bpm",
+    dest="get_bpm",
+    action="store_true",
+    help="Get tempo in beats per minute",
+)
+args = parser.parse_args()
 
-    audio_filepath = "./test_data/zimzalabim.mp3"
-    print(audio_filepath)
-    song = Song(audio_filepath)
-    print(song.get_estimated_song_key())
+print(f"Evaluating {args.audio_filepath}...")
+song = Song(args.audio_filepath, None if args.is_full_song else 10)
+if args.get_key:
+    print("Estimated Key:", song.get_estimated_song_key())
+if args.get_bpm:
+    print("Estimated Tempo:", round(song.get_tempo(), 2))
